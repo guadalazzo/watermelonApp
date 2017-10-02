67423b5b6202be7c415bb6f1c4c971759dc33835
guadalazzo <guadalazzo@est.frba.utn.edu.ar>
2017-10-01 21:59:03 -0300

    Added design and images to app

--- index.android.js
+++ index.android.js
@@ -9,44 +9,99 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  Image,
+  ScrollView,
 } from 'react-native';
 
 export default class demo extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Hola hugddasdasd
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
+         <Image style={styles.watermelon} source={require('./img/watermelon.png')}/>
+          <View style={styles.titleContainer}>
+            <Text style={styles.welcome}>
+              Watermelon for life 🍉 
+            </Text>
+          </View>  
+        <View style={styles.box}>
+          <Text style={styles.instructions}>
+            This is a sassy flavoured mobile 📱 app
+          </Text>
+          <ScrollView style={styles.gallery}>
+              <Image style={styles.img} source={require('./img/relojwatermelon.jpg')}/>
+              <Image style={styles.img} source={require('./img/monedero.jpg')}/>
+              <Image style={styles.img} source={require('./img/monedero.jpg')}/>
+              <Image style={styles.img} source={require('./img/monedero.jpg')}/>
+              <Image style={styles.img} source={require('./img/monedero.jpg')}/>
+          </ScrollView>
+        </View>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
+  watermelon: {
+    width: 50,
+    height: 50,
+  },
+  gallery: {
+    marginTop: 20,
+  },
+  img: {
+    borderWidth: 1,
+    borderColor:'#000',
+    borderRadius: 5,
+    flex: 1,
+    height: 350,
+    width: 300,
+    marginBottom: 30
+  },
+  titleContainer: {
+    backgroundColor: 'rgba(0,0,0,0.3)',
+    width: 300,
+    borderRadius: 10,
+    height: 50,
+    borderWidth:1,
+    borderColor: 'pink',
+
+  },
+  box:{
+    backgroundColor: '#338779',
+    width: 350,
+    height: 450,
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: '#fff',
+    shadowOffset: { width: 6, height: 7,},
+    shadowOpacity: 1,
+    shadowRadius: 9.51,
+    shadowColor:'#ffffff',
+    elevation: 15,
+  },
   container: {
     flex: 1,
-    justifyContent: 'center',
+
+    justifyContent: 'space-around',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#8c204b',
   },
   welcome: {
-    fontSize: 20,
+    fontWeight: 'bold',
+    color: 'lightgreen',
+    fontSize: 25,
     textAlign: 'center',
     margin: 10,
   },
   instructions: {
+    fontSize: 18,
     textAlign: 'center',
-    color: '#333333',
+    color: '#e2ffe5',
     marginBottom: 5,
+
   },
 });
 
