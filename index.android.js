eb7c8100548b365b5992e86367217efff0eb301d
guadalazzo <guadalazzo@est.frba.utn.edu.ar>
2018-04-21 00:29:02 -0300

    changed style from element

--- index.android.js
+++ index.android.js
@@ -18,57 +18,82 @@ export default class demo extends Component {
   render() {
     return (
       <View style={styles.container}>
-         <Image style={styles.watermelon} source={require('./img/watermelon.png')}/>
           <View style={styles.titleContainer}>
             <Text style={styles.welcome}>
-              Watermelon for life 🍉 
+               <Image style={styles.watermelon} source={require('./img/watermelon.png')}/>
+               Watermelon for life 
             </Text>
+            
           </View>  
-        <View style={styles.box}>
-          <Text style={styles.instructions}>
-            This is a sassy flavoured mobile 📱 app
-          </Text>
           <ScrollView style={styles.gallery}>
-              <Image style={styles.img} source={require('./img/relojwatermelon.jpg')}/>
-              <Image style={styles.img} source={require('./img/monedero.jpg')}/>
-              <Image style={styles.img} source={require('./img/monedero.jpg')}/>
-              <Image style={styles.img} source={require('./img/monedero.jpg')}/>
-              <Image style={styles.img} source={require('./img/monedero.jpg')}/>
+          <View style={styles.itemBox}>
+            <Image style={styles.img} source={require('./img/relojwatermelon.jpg')}/>
+            <Text style={styles.item}>Reloj Sandy 🍉</Text>
+          </View>
+          <View style={styles.itemBox}>
+            <Image style={styles.img} source={require('./img/monedero.jpg')}/>
+            <Text style={styles.item}>Reloj Sandy 🍉</Text>
+          </View>
           </ScrollView>
-        </View>
+          <Text style={styles.welcome}>Made by Lupe</Text>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  watermelon: {
-    width: 50,
-    height: 50,
+  item: {
+    fontSize: 16,
+    color: '#000',
+    textAlign: 'center',
+    marginBottom: 5,
+  },
+  itemBox: {
+    alignItems:'center',
+    justifyContent:'center',
+    width: 400,
+    height: 300,
+    borderColor: '#f5f5dc',
   },
   gallery: {
     marginTop: 20,
+    width: 400,
   },
   img: {
     borderWidth: 1,
-    borderColor:'#000',
+    //borderColor:'#000',
     borderRadius: 5,
     flex: 1,
     height: 350,
     width: 300,
-    marginBottom: 30
+    margin: 10,
   },
   titleContainer: {
-    backgroundColor: 'rgba(0,0,0,0.3)',
-    width: 300,
-    borderRadius: 10,
-    height: 50,
+    justifyContent: 'space-around',
+    backgroundColor: '#fff',
+    width: 500,
+    alignItems: 'center',
+    height: 60,
     borderWidth:1,
     borderColor: 'pink',
 
   },
+  watermelon: {
+    marginTop: 10,
+    marginBottom: 10,
+    width: 60,
+    height: 60,
+  },
+  welcome: {
+    width: 300,
+    fontWeight: 'bold',
+    color: '#de8f84',
+    fontSize: 25,
+    textAlign: 'center',
+    //margin: 10,
+    justifyContent: 'space-between',
+  },
   box:{
-    backgroundColor: '#338779',
     width: 350,
     height: 450,
     justifyContent: 'space-around',
@@ -84,24 +109,16 @@ const styles = StyleSheet.create({
   },
   container: {
     flex: 1,
-
     justifyContent: 'space-around',
     alignItems: 'center',
-    backgroundColor: '#8c204b',
+    backgroundColor: '#ffeded',
   },
-  welcome: {
-    fontWeight: 'bold',
-    color: 'lightgreen',
-    fontSize: 25,
-    textAlign: 'center',
-    margin: 10,
-  },
+  
   instructions: {
     fontSize: 18,
     textAlign: 'center',
     color: '#e2ffe5',
     marginBottom: 5,
-
   },
 });
 
